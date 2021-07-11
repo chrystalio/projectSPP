23a02a8736718c353a9d0236e28c4befca1c9b8d
Chrystalio <82774717+chrystalio@users.noreply.github.com>
2021-07-11 23:21:21 +0700

    Membuat Template Admin dashboard projectSPP

---
+++ db/conn.php
@@ -0,0 +1,10 @@
+<?php
+
+    $host = '127.0.0.1';
+    $user = 'root';
+    $pass = '';
+    $db   = 'db_spp';
+
+    $conn = new mysqli($host, $user, $pass, $db) or die('koneksi gagal!');
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<?php
-echo "tes";
+<?php require_once 'includes/header.php'; ?>
+<?php require_once 'includes/sidebar.php'; ?>
+<?php require_once 'includes/navbar.php'; ?>
 
-?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+<?php require_once 'includes/footer.php'; ?>
